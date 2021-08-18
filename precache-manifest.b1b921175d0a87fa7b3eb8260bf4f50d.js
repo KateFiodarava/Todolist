35b4def750ab7ecdde85a0863b7af05e59fa89ca
KateFiodarava <apanasevech@mail.ru>
2021-08-18 23:39:38 +0300

    Updates

---
+++ precache-manifest.b1b921175d0a87fa7b3eb8260bf4f50d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f25bb7f75ccc55260974840a4d1fdb",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "4e5c409a4ac36c0ff3a9",
+    "url": "/Todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "8dd4d1d576484c1409ce",
+    "url": "/Todolist/static/js/2.96e72576.chunk.js"
+  },
+  {
+    "revision": "936711c3f4be0c9c88207c43cda44117",
+    "url": "/Todolist/static/js/2.96e72576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e5c409a4ac36c0ff3a9",
+    "url": "/Todolist/static/js/main.0932df30.chunk.js"
+  },
+  {
+    "revision": "23a8ead80f8150e06955",
+    "url": "/Todolist/static/js/runtime-main.ba498293.js"
+  }
+]);
